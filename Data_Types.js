9d8b8ecbc5462208dcfd55fc9c5a5b236fab0236
Muhammad-Rebaal <122050424+Muhammad-Rebaal@users.noreply.github.com>
2023-04-04 00:23:21 +0500

    Add files via upload

---
+++ Data_Types.js
@@ -0,0 +1,24 @@
+// single line comment
+/*
+multi
+line 
+comment
+
+*/
+// var num = 1
+// var num = 2
+// let numb = 2
+// numb = 3
+// const number = 5
+// console.log(num,numb,number)
+
+console.log(typeof 5)
+console.log(typeof "string")
+console.log(typeof Symbol())
+console.log(typeof 10n)
+console.log(typeof undefined)
+console.log(typeof null)
+
+console.log(typeof {})
+console.log(typeof [])
+
